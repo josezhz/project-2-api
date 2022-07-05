995c9fe11cac0817a36fe12620187c692447610c
josezhz <zhang.haozhe@trentglobal.edu.sg>
2022-07-05 09:32:12 +0000

    Allowed READ with no criteria

--- index.js
+++ index.js
@@ -14,13 +14,40 @@ async function main() {
     await MongoUtil.connect(MONGO_URI, "project_2");
     const db = MongoUtil.getDB();
 
+    const allCharacters = await db.collection("characters").find({}).toArray();
+    const allWeapons = await db.collection("weapons").find({}).toArray();
+    const allArtifacts = await db.collection("artifacts").find({}).toArray();
+    const allBosses = await db.collection("bosses").find({}).toArray();
+
     app.get("/", function (req, res) {
         res.send("Hello World");
     });
 
     app.get("/teams", async function (req, res) {
-        let teams = await db.collection("teams").find({}).toArray();
+        let teamName = req.body.teamName;
+        let numberOfFiveStar = req.body.numberOfFiveStars;
+        let includedCharacters = req.body.includedCharacters;
+        let recommendedBosses = req.body.recommendedBosses;
+
+        let criteria = {};
+        let teams = await db.collection("teams").find(criteria).toArray();
         res.json({ teams });
+    });
+
+    app.post("/teams", async function (req, res) {
+        let team_name = req.body.team_name;
+        let team_composition = req.body.team_composition;
+        let bosses = req.body.bosses;
+        let rotation_guide = req.body.rotation_guide;
+        let notes= req.body.notes;
+        await db.collection("teams").insertOne({
+            team_name,
+            team_composition,
+            bosses,
+            rotation_guide,
+            notes
+        });
+        res.sendStatus(200);
     });
 }
 main();
